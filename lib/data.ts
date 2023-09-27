39f99acf9435aaa5fda2db11e6d4d695149aa252
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-27 18:41:42 +0530

    Added constant data

---
+++ lib/data.ts
@@ -0,0 +1,49 @@
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Azure",
+  "Docker",
+  "Terraform",
+  "Tailwind",
+  "MongoDB",
+  "Redux",
+  "Express",
+  "PostgreSQL",
+  "Java",
+  "Spring boot",
+  "Python",
+  "Flask",
+] as const;
