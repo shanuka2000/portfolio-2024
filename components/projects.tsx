3c80542347709c85318805844030d53a4c7a28c7
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-29 01:24:57 +0530

    Implemented nav active section transform

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
 import { projectData } from "@/lib/data";
 import Project from "@/components/project";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView, setActiveSection]);
+
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectData.map((project, index) => (
