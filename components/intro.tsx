dc00946b1bde2298398e4462220967b4dc686a7f
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-28 16:19:39 +0530

    Removed hardcoded image size in intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -25,8 +25,6 @@ export default function Intro() {
             <Image
               src={profileImage}
               alt="Shanuka Profile"
-              width="192"
-              height="192"
               quality="95"
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
