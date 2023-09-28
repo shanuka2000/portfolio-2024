1e7c1b21851a59f982b06237f457037f72587bea
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-29 01:58:18 +0530

    Refactored SectionName type from global types file

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import React, { createContext, useContext, useState } from "react";
-import { links } from "@/lib/data";
-
-type SectionName = (typeof links)[number]["name"];
+import type { SectionName } from "@/lib/types";
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
