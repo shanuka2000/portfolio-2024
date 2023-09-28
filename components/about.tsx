f5f12c5da183f80368d372eefdb3653b9bc4886b
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-29 01:37:06 +0530

    Added observer disable to current components

--- components/about.tsx
+++ components/about.tsx
@@ -8,13 +8,13 @@ import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function About() {
   const { ref, inView } = useInView({ threshold: 0.75 });
-  const { setActiveSection } = useActiveSectionContext();
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView) {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection("About");
     }
-  }, [inView, setActiveSection]);
+  }, [inView, setActiveSection, timeOfLastClick]);
 
   return (
     <motion.section
