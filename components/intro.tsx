d2fa64488a96a056621e7c7fd30051d6656dc3d0
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-29 23:10:17 +0530

    Added common border class

--- components/intro.tsx
+++ components/intro.tsx
@@ -85,7 +85,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          className="group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10"
+          className="group cursor-pointer bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition borderBlack"
           href="/CV.pdf"
           download
         >
@@ -93,14 +93,14 @@ export default function Intro() {
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
         <a
-          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-[1.15] hover:text-gray-950 active:scale-[1.15] transition border border-black/10"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-[1.15] hover:text-gray-950 active:scale-[1.15] transition borderBlack"
           href="https://linkedin.com/in/shanuka-peiris/"
           target="_blank"
         >
           <BsLinkedin />
         </a>
         <a
-          className="bg-white p-4 text-gray-700  flex items-center gap-2 !text-[1.35rem] rounded-full focus:scale-110 hover:scale-[1.15] hover:text-gray-950 active:scale-[1.15] transition border border-black/10"
+          className="bg-white p-4 text-gray-700  flex items-center gap-2 !text-[1.35rem] rounded-full focus:scale-110 hover:scale-[1.15] hover:text-gray-950 active:scale-[1.15] transition borderBlack"
           href="https://github.com/shanuka2000"
           target="_blank"
         >
