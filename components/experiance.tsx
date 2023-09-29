974e406cda3f3f9685eb8793c05469ff3f4f98d8
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-29 22:59:26 +0530

    Removed threshold parameter

--- components/experiance.tsx
+++ components/experiance.tsx
@@ -11,7 +11,7 @@ import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function Experiance() {
-  const { ref } = useSectionInView("Experience", 0.5);
+  const { ref } = useSectionInView("Experience");
 
   return (
     <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
