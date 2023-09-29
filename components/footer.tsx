94c173712763dbcea42941c108c04c911cd21d84
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-30 03:07:53 +0530

    Implemented footer component

---
+++ components/footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500">
+      <small className="mb-2 block text-xs">
+        &copy; 2024 Shanuka. All rights reserved.
+      </small>
+      <p className="text-xs">
+        <span className="font-semibold">About this website: </span>
+        build with React & Next.js (App Router & Server Actions), Typescript,
+        Tailwind CSS, Framer Motion, React Email & Resend, Netlify hosting.
+      </p>
+    </footer>
+  );
+}
