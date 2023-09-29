d6c296fdff4179837139f85dda11f1948ed01ded
shanuka2000 <shanuka.inbox@gmail.com>
2023-09-30 02:59:59 +0530

    Added toast notification for form submission

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,6 +6,7 @@ import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmail } from "@/actions/sendEmail";
 import SubmitBtn from "./submit-btn";
+import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -32,7 +33,14 @@ export default function Contact() {
       <form
         className="mt-10 flex flex-col"
         action={async (formData) => {
-          await sendEmail(formData);
+          const { data, error } = await sendEmail(formData);
+
+          if (error) {
+            toast.error(error);
+            return;
+          }
+
+          toast.success("Email sent successfully!");
         }}
       >
         <input
